580734028e6340140312fecf1cbbe6912b1b4d9f
xuwen nie <niexuwen@tac10.wpa.dal.ca>
2015-10-22 15:43:30 -0300

    Initial Commit

---
+++ a5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  a5
+//
+//  Created by xuwen nie on 15/10/22.
+//  Copyright © 2015年 xuwen nie. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
